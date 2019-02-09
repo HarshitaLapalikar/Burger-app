8af08d935340f871975f7d28bc591be84ea0e7ba
Harshitalapalikar <harshitalapalikar@gmail.com>
2019-02-09 12:11:29 +0530

    BAsic syntax and use of REdux- Action, Reducer, Store, Subscription

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route , Switch} from 'react-router-dom'
+import { CreateStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
